391279e39372cd06233ed5b5db8bff2ebf2e45d5
clayton <claytonccollins@gmail.com>
2017-02-22 13:19:06 -0800

    restaurant passes getbyid method test

--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -87,4 +87,31 @@
 
             $this->assertEquals($result, []);
         }
+
+        function test_getById()
+        {
+            $cuisine_id = 3;
+            $name = 'greek geeks';
+            $spice = 2;
+            $price = 3;
+            $size = 4;
+            $review = 'service was ok, but water had lead in it';
+            $id = null;
+            $test_restaurant = new Restaurant($cuisine_id, $name, $spice, $price, $size, $review, $id);
+            $test_restaurant->save();
+
+            $cuisine_id2 = 2;
+            $name2 = 'roman times';
+            $spice2 = 1;
+            $price2 = 2;
+            $size2 = 1;
+            $review2 = 'large portions, poor quality';
+            $id2 = null;
+            $test_restaurant2 = new Restaurant($cuisine_id2, $name2, $spice2, $price2, $size2, $review2, $id2);
+            $test_restaurant2->save();
+
+            $result = Restaurant::getById($test_restaurant2->getId());
+
+            $this->assertEquals($result, $test_restaurant2);
+        }
     }
